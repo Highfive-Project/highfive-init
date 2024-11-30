bcc7404847165abc3ab94279e6f3ea2db0e82b0e
jasmaa <jasonmaa3955@gmail.com>
2024-11-29 19:35:33 -0500

    test: Add unit test

---
+++ bootstrap.test.ts
@@ -0,0 +1,37 @@
+import { beforeEach, expect, it, vi } from 'vitest';
+import { fs, vol } from 'memfs';
+import mock from 'mock-require';
+
+mock('node:fs', {
+  ...require('node:fs'),
+  mkdirSync: fs.mkdirSync,
+  writeFileSync: fs.writeFileSync,
+  existsSync: fs.existsSync,
+  renameSync: fs.renameSync,
+});
+
+mock('node:readline', {
+  createInterface: () => {
+    return {
+      question: vi.fn().mockImplementation((_question, cb) => {
+        cb("dummy");
+      }),
+      close: vi.fn(),
+    };
+  }
+});
+
+const bootstrap = require('./bootstrap');
+
+beforeEach(() => {
+  vol.reset();
+});
+
+it('creates project directory', async () => {
+  await bootstrap.main();
+
+  expect(fs.existsSync("./highlightjs-dummy")).toBeTruthy();
+  expect(fs.lstatSync("./highlightjs-dummy").isDirectory()).toBeTruthy();
+  expect(fs.readFileSync("./highlightjs-dummy/README.md", "utf8").includes("highlightjs-dummy")).toBeTruthy();
+});
+
